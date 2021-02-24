66ade0a7f7ef9a20d3737bb8d8bb9a614f592fe0
RajeshkannanBalu <RAJESHKANNAN.BALU@target.com>
2021-02-24 09:13:47 +0530

    creating coupons client jar file

---
+++ backpack-registry-coupons-service/src/main/kotlin/com/tgt/backpackregistrycoupons/transport/PromoCouponRedemptionTO.kt
@@ -0,0 +1,41 @@
+package com.tgt.backpackregistrycoupons.transport
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.util.*
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class PromoCouponRedemptionTO(
+    @JsonProperty("location_id")
+    val storeId: String? = null,
+    @JsonProperty("coupon_code")
+    val couponCode: String,
+    @JsonProperty("coupon_type")
+    val couponType: String? = null,
+    @JsonProperty("status")
+    val status: String? = null,
+    @JsonProperty("transaction_time")
+    val transactionTime: String? = null,
+    @JsonProperty("register_id")
+    val registerId: String? = null,
+    @JsonProperty("transaction_id")
+    val tranId: String? = null,
+    @JsonProperty("registry_id")
+    val registryId: UUID? = null
+) {
+    companion object {
+        private val jsonMapper = jacksonObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+
+        @JvmStatic
+        fun deserialize(byteArray: ByteArray): PromoCouponRedemptionTO {
+            return jsonMapper.readValue(byteArray, PromoCouponRedemptionTO::class.java)
+        }
+
+        @JvmStatic
+        fun serialize(promoCouponRedemptionTO: PromoCouponRedemptionTO): ByteArray {
+            return jsonMapper.writeValueAsBytes(promoCouponRedemptionTO)
+        }
+    }
+}
