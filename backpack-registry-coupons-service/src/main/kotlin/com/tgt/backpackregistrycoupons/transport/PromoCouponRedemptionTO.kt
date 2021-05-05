e5a8372cf20f263989b24631073bb5a68054d373
RajeshkannanBalu <RAJESHKANNAN.BALU@target.com>
2021-05-05 17:23:28 +0530

    Update PromoCouponRedemptionTO.kt (#72)
    * Update PromoCouponRedemptionTO.kt
    
    adding time zone
    
    * Update PromoCouponRedemptionTO.kt

--- backpack-registry-coupons-service/src/main/kotlin/com/tgt/backpackregistrycoupons/transport/PromoCouponRedemptionTO.kt
+++ backpack-registry-coupons-service/src/main/kotlin/com/tgt/backpackregistrycoupons/transport/PromoCouponRedemptionTO.kt
@@ -19,7 +19,7 @@ data class PromoCouponRedemptionTO(
     val status: String? = null,
     @JsonDeserialize(using = LocalDateTimeDeserializer::class)
     @JsonSerialize(using = LocalDateTimeSerializer::class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:SSSZ")
     val transactionTime: LocalDateTime? = null,
     val registerId: String? = null,
     val transactionId: String? = null,
