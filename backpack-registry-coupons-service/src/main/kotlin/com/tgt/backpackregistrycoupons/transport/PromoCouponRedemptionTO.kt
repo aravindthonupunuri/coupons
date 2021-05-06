1a2bd323cae48eddce8d0ec54a1ce9129c2097bb
MohanaSundaram <mohanasundaram.a@target.com>
2021-05-06 16:21:12 +0530

    Fixing transaction_date parse issue

--- backpack-registry-coupons-service/src/main/kotlin/com/tgt/backpackregistrycoupons/transport/PromoCouponRedemptionTO.kt
+++ backpack-registry-coupons-service/src/main/kotlin/com/tgt/backpackregistrycoupons/transport/PromoCouponRedemptionTO.kt
@@ -1,14 +1,8 @@
 package com.tgt.backpackregistrycoupons.transport
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import java.time.LocalDateTime
 import java.util.*
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -17,10 +11,7 @@ data class PromoCouponRedemptionTO(
     val couponCode: String,
     val couponType: String? = null,
     val status: String? = null,
-    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
-    @JsonSerialize(using = LocalDateTimeSerializer::class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    val transactionTime: LocalDateTime? = null,
+    val transactionTime: String? = null,
     val registerId: String? = null,
     val transactionId: String? = null,
     val registryId: UUID
