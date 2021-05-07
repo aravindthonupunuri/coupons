c6df89ec73b4b40785653d85c750b2b09ea2b4a2
RajeshkannanBalu <RAJESHKANNAN.BALU@target.com>
2021-05-07 16:16:54 +0530

    adding join

--- backpack-registry-coupons-service/src/main/kotlin/com/tgt/backpackregistrycoupons/persistence/repository/registrycoupons/RegistryCouponsRepository.kt
+++ backpack-registry-coupons-service/src/main/kotlin/com/tgt/backpackregistrycoupons/persistence/repository/registrycoupons/RegistryCouponsRepository.kt
@@ -3,6 +3,7 @@ package com.tgt.backpackregistrycoupons.persistence.repository.registrycoupons
 import com.tgt.backpackregistrycoupons.domain.model.RegistryCoupons
 import com.tgt.backpackregistrycoupons.util.CouponType
 import com.tgt.backpackregistrycoupons.util.CouponRedemptionStatus
+import io.micronaut.data.annotation.Join
 import io.micronaut.data.annotation.Query
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -16,6 +17,7 @@ interface RegistryCouponsRepository {
 
     fun saveAll(registryCoupons: List<RegistryCoupons>): Flux<RegistryCoupons>
 
+    @Join(value = "registry", type = Join.Type.FETCH)
     fun findByCouponCode(couponCode: String): Mono<RegistryCoupons>
 
     @Query("UPDATE registry_coupons SET Coupon_redemption_status = (:CouponRedemptionStatus) WHERE coupon_code=(:couponCode)")
